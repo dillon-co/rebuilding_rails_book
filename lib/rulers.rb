38968c9a1f8cbd5238b9629f17d50169fe139d5b
Dillon <dilloncortez@gmail.com>
2019-09-16 16:07:12 -0600

    added server and debug file

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,5 +2,11 @@ require "rulers/version"
 
 module Rulers
   class Error < StandardError; end
-  # Your code goes here...
+  class Application
+    def call(env)
+      `echo debug > debug.txt`;
+      [200, {'Content-Type' => 'text/html'},
+        ["Hello From Ruby On Rulers!"]]
+    end
+  end
 end

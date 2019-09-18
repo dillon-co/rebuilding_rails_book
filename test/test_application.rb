2182837a78dc9dc50085e55aa42fc6ecaa4e1cb7
Dillon <dilloncortez@gmail.com>
2019-09-18 13:43:09 -0600

    Added basic routing

---
+++ test/test_application.rb
@@ -0,0 +1,24 @@
+require_relative 'test_helper'
+
+class TestApp < Rulers::Application
+end
+
+class RulersAppTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    TestApp.new
+  end
+
+  def test_request
+    get "/"
+
+    assert last_response.ok?
+    body = last_response.body
+    assert body['Hello']
+  end
+
+  def test_array
+    assert [1,2,3,4].sum == 10
+  end
+end
